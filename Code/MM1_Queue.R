4bd1cadf56382aeb564e928ed8fb67fbd4cb740f
Anmol Sahoo <anmol.sahoo@gmail.com>
2016-10-18 02:58:03 +0530

    Started the MM1 queue code

---
+++ Code/MM1_Queue.R
@@ -0,0 +1,8 @@
+# Code to simulate a M/M/1 Queue
+
+# Defining constants
+tend = 10^5    # Time for end of simulation
+lambda = 1     # Arrival rate
+mu = 4/3       # Service rate
+
+# Running the simulation
