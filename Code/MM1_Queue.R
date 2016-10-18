ed970468943e66ae4df7b361f7f79bafdcd38783
Harshit Garg <Harshit Garg>
2016-10-18 12:31:52 +0530

    MM1 queue with little's law proof

--- Code/MM1_Queue.R
+++ Code/MM1_Queue.R
@@ -1,46 +1,34 @@
 # Code to simulate a M/M/1 Queue
 
 # Defining constants
-tend = 1000    # Time for end of simulation
-lambda = 1     # Arrival rate
+duration = 1000    # Duration of simulation
+lambda = 1.25     # Arrival rate
 mu = 4/3       # Service rate
 
 # Creating lists to store values
 arrivalTime = c()
 entryTime = c()
 exitTime = c()
-serviceTime = c()
-interarrivalTime = c()
 residentTime = c()
 
 # Defining runtime variables
 k = 1        # kth arrival
-t = 0.0      # Simulation time
 
 # Running the simulation
 
 # First arrival
-interarrivalTime[k] = rexp(1,lambda)
-arrivalTime[k] = t + interarrivalTime[k]
-serviceTime[k] = rexp(1,mu)
-entryTime[k] = arrivalTime[k]
+arrivalTime[k] = rexp(1,lambda)
+entryTime[k] = rexp(1,mu)
 exitTime[k] = entryTime[k] + serviceTime[k]
 
-while(exitTime[k] < tend) {
- # print(k)
-k = k + 1
-interarrivalTime[k] = rexp(1,lambda)
-serviceTime[k] = rexp(1,mu)
-
-arrivalTime[k] = arrivalTime[k-1] + interarrivalTime[k]
-
-if(arrivalTime[k] < exitTime[k-1]) {
-  entryTime[k] = exitTime[k-1]
-  exitTime[k] = entryTime[k] + serviceTime[k]
-} else{
-  entryTime[k] = arrivalTime[k]
-  exitTime[k] = entryTime[k] + serviceTime[k]
+while(exitTime[k] < duration) {
+  k = k + 1
+  arrivalTime[k] = arrivalTime[k-1] + rexp(1,lambda)
+  entryTime[k] <- if(arrivalTime[k] < exitTime[k-1])
+    exitTime[k-1]
+  else
+    arrivalTime[k]
+  exitTime[k] = entryTime[k] + rexp(1,mu) 
 }
-}
+
 residentTime = exitTime - arrivalTime
-print(mean(residentTime))
